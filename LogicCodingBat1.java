89150818a22c602e7c9d44d02f10a836e308db38
aden-tyler <alohaaden123@gmail.com>
2024-10-17 11:18:56 -1000

    Update LogicCodingBat1.java

--- LogicCodingBat1.java
+++ LogicCodingBat1.java
@@ -1,5 +1,9 @@
-//cigarParty
-//When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
+/** Task 1: cigarParty
+* When squirrels get together for a party, they like to have cigars. 
+* A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. 
+* Unless it is the weekend, in which case there is no upper bound on the number of cigars. 
+* Return true if the party with the given values is successful, or false otherwise.
+**/
 if (cigars >= 40)
   {
       if (cigars <= 60)
