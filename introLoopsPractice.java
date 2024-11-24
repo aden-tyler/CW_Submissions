39fa059dd2d56e1ef5cfb7c332663df111956800
aden-tyler <alohaaden123@gmail.com>
2024-11-24 11:30:00 -1000

    Update introLoopsPractice.java

--- introLoopsPractice.java
+++ introLoopsPractice.java
@@ -5,7 +5,7 @@ public class introLoopsPractice {
 		// TODO Auto-generated method stub
 		System.out.println(countLetters("aajandsngasnanansdgnds", 'a'));
 		System.out.println(getFactorial(5));
-		System.out.println(getIfRepeats("bdedd", "a"));
+		System.out.println(getIfRepeats("abcde"));
 	}
 	/**
 	 * This method counts the amount of a certain character 
@@ -48,24 +48,17 @@ public class introLoopsPractice {
 	 * @param letter
 	 * @return
 	 */
-	public static boolean getIfRepeats(String str, String letter)
+	public static boolean getIfRepeats(String str )
 	{
-		for (int check1 = 0; check1 < str.length(); check1++)
+		for (int check1 = 0; check1 < str.length() - 1 ; check1++)
 		{
 			//Examines the given input again
-			for(int check2 = check1; check2 < str.length(); check2++)
-			{
 		
 				//Returns true if the examined characters match any of the other characters.
-				if (str.charAt(check1) == str.charAt(check2))
+				if (str.charAt(check1) == str.charAt(check1+1))
 				{
 					return true;
 				}
-				else if (str.charAt(check1) != str.charAt(check2))
-				{
-					return false;
-				}
-			}
 			
 		}
 		return false;
